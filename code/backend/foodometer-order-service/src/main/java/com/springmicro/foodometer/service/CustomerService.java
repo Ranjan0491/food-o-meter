498dfa89aff7d10221dff2b3b476d4893112be1b
Ranjan0491 <04ranjansaha91@gmail.com>
2021-08-30 14:40:36 +0530

    adding new address for customer

--- code/backend/foodometer-order-service/src/main/java/com/springmicro/foodometer/service/CustomerService.java
+++ code/backend/foodometer-order-service/src/main/java/com/springmicro/foodometer/service/CustomerService.java
@@ -12,6 +12,7 @@ import com.springmicro.foodometer.web.mapper.CustomerMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -58,6 +59,45 @@ public class CustomerService {
                 throw new CustomerException("No customer found with id - "+id);
         } else {
             throw new CustomerException("Customer Id is missing");
+        }
+    }
+
+    public List<AddressDto> getCustomerAddress(String id) {
+        CustomerDto customerDto = getCustomerById(id);
+        return customerDto.getAddresses();
+    }
+
+    public AddressDto getCustomerAddressById(String customerId, String addressId) {
+        List<AddressDto> addressDtos = getCustomerAddress(customerId);
+        Optional<AddressDto> optionalAddress = addressDtos.stream().filter(addressDto -> addressDto.getId().equals(addressId)).findFirst();
+        if(optionalAddress.isPresent()) {
+            return optionalAddress.get();
+        } else {
+            throw new CustomerException("No Address exists with id - "+addressId);
+        }
+    }
+
+    public void saveNewAddressForCustomer(String customerId, AddressDto addressDto) {
+        CustomerDto customerDto = getCustomerById(customerId);
+        if(customerDto.getAddresses().stream().anyMatch(address -> {
+            if(StringUtils.equalsIgnoreCase(address.getLine1(), addressDto.getLine1()) &&
+                    StringUtils.equalsIgnoreCase(address.getLine2(), addressDto.getLine2()) &&
+                    StringUtils.equalsIgnoreCase(address.getLine3(), addressDto.getLine3()) &&
+                    StringUtils.equalsIgnoreCase(address.getCity(), addressDto.getCity()) &&
+                    StringUtils.equalsIgnoreCase(address.getState(), addressDto.getState()) &&
+                    StringUtils.equalsIgnoreCase(address.getPinCode(), addressDto.getPinCode())) {
+                return true;
+            } else {
+                return false;
+            }
+        })) {
+            log.debug("Address found for Customer with id : "+customerId);
+            throw new CustomerException("Address already exists - "+addressDto);
+        } else {
+            log.debug("Address not found for Customer with id : "+customerId+" (Saving new address)");
+            Customer customer = customerRepository.findById(customerId).get();
+            customer.getAddresses().add(addressMapper.addressDtoToAddress(addressDto));
+            customerRepository.save(customer);
         }
     }
 }
--- code/backend/foodometer-order-service/src/main/java/com/springmicro/foodometer/web/controller/CustomerController.java
+++ code/backend/foodometer-order-service/src/main/java/com/springmicro/foodometer/web/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.springmicro.foodometer.web.controller;
 
 import com.springmicro.foodometer.service.CustomerService;
+import com.springmicro.foodometer.web.dto.AddressDto;
 import com.springmicro.foodometer.web.dto.CustomerDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -26,5 +27,23 @@ public class CustomerController {
     public ResponseEntity<CustomerDto> getCustomerById(@PathVariable("id") String id) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(customerService.getCustomerById(id));
+    }
+
+    @GetMapping("/{id}/addresses")
+    public ResponseEntity<List<AddressDto>> getCustomerAddress(@PathVariable("id") String id) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(customerService.getCustomerAddress(id));
+    }
+
+    @GetMapping("/{customerId}/addresses/{addressId}")
+    public ResponseEntity<AddressDto> getCustomerAddressById(@PathVariable("customerId") String customerId, @PathVariable("addressId") String addressId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(customerService.getCustomerAddressById(customerId, addressId));
+    }
+
+    @PutMapping("/{id}/addresses")
+    public ResponseEntity<Void> saveCustomerAddress(@PathVariable("id") String id, @RequestBody AddressDto addressDto) {
+        customerService.saveNewAddressForCustomer(id, addressDto);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
